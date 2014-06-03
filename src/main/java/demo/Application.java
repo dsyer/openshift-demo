95c7855e36168a8f94f2ba91bf6d7ca72a14291b
Dave Syer <dsyer@gopivotal.com>
2014-06-03 14:34:08 +0100

    Add hooks

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,16 +1,32 @@
 package demo;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@Controller
 public class Application {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, String> hello() {
+		return Collections.singletonMap("message",
+				"Hello World");
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
